c6e7e54b1c52f90f06be3cd8dfe21e92e9d40e1d
naaspati <naaspati@gmail.com>
2019-02-04 09:24:19 +0530

    minor

---
+++ 778-JEETT/009-dukeetf/src/main/java/javaeetutorial/hello1/Hello.java
@@ -0,0 +1,17 @@
+package javaeetutorial.hello1;
+
+import java.awt.Desktop;
+
+import javax.enterprise.inject.Model;
+
+@Model
+public class Hello {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
